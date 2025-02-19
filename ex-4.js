1e91a7dd5a2ae76184445a43f17faf707c93b9bb
Gett <phonlachet.t@gmail.com>
2025-02-19 21:42:40 +0700

    Submit

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,19 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+};
+
+let addResult = calculator.add(10, 20);
+console.log(addResult);
+
+let subtractResult = calculator.subtract(1000, 300);
+console.log(subtractResult);
+
+let multiplyResult = calculator.multiply(100, 20);
+console.log(multiplyResult);
+
+let divideResult = calculator.divide(3000, 10);
+console.log(divideResult);
